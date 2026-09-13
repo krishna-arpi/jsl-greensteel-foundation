dccff2dd4c9b15e3b1d08e46b6efef7375d61288
Tejas Mishra <06tejasmishra06@gmail.com>
2026-09-13 10:02:43 +0530

    fix: add router basename for GitHub Pages sub-path

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/jsl-greensteel-foundation">
       <App />
     </BrowserRouter>
   </React.StrictMode>
